314d8eae9b901c84d4080220dd40d91a76778148
Egidiu <egidiugabor@gmail.com>
2023-03-26 21:43:40 +0200

    caioo

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,21 +1,10 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-import NetlifyCMS from 'astro-netlify-cms';
 
 import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://example.com',
-	integrations: [mdx(), sitemap(), NetlifyCMS({
-		config: {
-		  backend: {
-			name: 'git-gateway',
-			branch: 'main',
-		  },
-		  collections: [
-			// Content collections
-		  ],
-		},
-	  })],
+	integrations: [mdx(), sitemap()],
 });
